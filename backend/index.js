c24a960df5ffed08f6d42fb1579169d4eadeb594
Ked57 <kedketh@gmail.com>
2019-10-03 10:09:52 +0200

    Working backend with github api querying

--- backend/index.js
+++ backend/index.js
@@ -1,71 +1,95 @@
 const Koa = require("koa");
 const pMemoize = require("p-memoize");
 const fetch = require("node-fetch");
+const { gql } = require("apollo-boost");
+const { default: ApolloClient } = require("apollo-boost");
 const app = new Koa();
 
 const TTL = process.env.TTL || 1000;
-const githubEndpoint =
-  process.env.GITHUB_ENDPOINT || "https://api.github.com/graphql";
 const GITHUB_OAUTH = process.env.GITHUB_OAUTH || "";
 
-const generateQuery = cursor => `query getOpenedPRsForOrganization($login: String! ${
-  cursor ? "$cursor: String!" : ""
-}) {
-    organization(login: $login) {
-      membersWithRole(first: 100 ${cursor ? "after: $cursor" : ""}) {
-        nodes {
-            name
-          login
-          avatarUrl
-          pullRequests(states: OPEN, before: "2019-10-31", after: "2019-10-01") {
-            totalCount
-          }
-        }
-         pageInfo {
-            endCursor
-            hasNextPage
-          }
-      }
-    }
-  }`;
-const generateVariables = (login, cursor) => `variables {
-    "login": ${login}${
-  cursor
-    ? `,
-        "cursor": ${cursor}`
-    : ""
-}
-  }
-  `;
+const client = new ApolloClient({
+  uri: `https://api.github.com/graphql?access_token=${GITHUB_OAUTH}`,
+  fetch
+});
 
 const fetchGithub = async organization => {
-  let hasNextPage = true;
-  let cursor;
-  const res = {};
-  while (hasNextPage) {
-    console.log(generateQuery(organization, cursor));
-    const response = await fetch(githubEndpoint, {
-      method: "POST",
-      body: {
-        query: generateQuery(cursor),
-        variables: generateVariables(organization, cursor)
-      },
-      headers: { Authorization: `Bearer ${GITHUB_OAUTH}` }
+  const res = [];
+  let pageInfo;
+  do {
+    const result = await client.query({
+      query: gql`
+        query getOpenedPRsForOrganization($login: String!, $cursor: String) {
+          organization(login: $login) {
+            membersWithRole(first: 50, after: $cursor) {
+              nodes {
+                name
+                login
+                avatarUrl
+                pullRequests(states: [OPEN, CLOSED, MERGED], last: 25) {
+                  nodes {
+                    createdAt
+                    title
+                    repository {
+                      name
+                      isPrivate
+                    }
+                  }
+                }
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        login: organization,
+        cursor: pageInfo && pageInfo.endCursor ? pageInfo.endCursor : undefined
+      }
     });
-    const result = await response.json();
-    // hasNextPage = result.organization.membersWithRoles.pageInfo.hasNextPage;
-    // cursor = result.organization.membersWithRoles.pageInfo.cursor;
-    hasNextPage = false;
-    Object.assign(res, { ...res, ...result });
-  }
-  console.log(res);
+    res.push(...result.data.organization.membersWithRole.nodes);
+    pageInfo = result.data.organization.membersWithRole.pageInfo;
+  } while (pageInfo.hasNextPage);
   return res;
 };
 
-const memFetchGithub = pMemoize(fetchGithub, { maxAge: TTL });
+const computeGithubData = data =>
+  data
+    .map(row => ({
+      name: row.name,
+      login: row.login,
+      avatarUrl: row.avatarUrl,
+      pullRequests: row.pullRequests.nodes.filter(pr => {
+        const createDate = new Date(Date.parse(pr.createdAt));
+        const currentDate = new Date(Date.now());
+        return (
+          !pr.isPrivate &&
+          createDate.getMonth() === currentDate.getMonth() &&
+          createDate.getFullYear() === currentDate.getFullYear()
+        );
+      })
+    }))
+    .map(row => ({
+      ...row,
+      count: row.pullRequests.length
+    }))
+    .filter(row => {
+      return row.pullRequests.length >= 1;
+    })
+    .sort((row1, row2) => row2.count - row1.count);
+
+const fetchAndComputeGithubData = async organization =>
+  computeGithubData(await fetchGithub(organization));
+
+const memFetchGithub = pMemoize(fetchAndComputeGithubData, { maxAge: TTL });
 
 app.use(async ctx => {
   ctx.body = await memFetchGithub("Zenika");
 });
 
-app.listen(3000);
+const server = app.listen(3000);
+
+console.log(`Server started on port ${server.address().port}`);
