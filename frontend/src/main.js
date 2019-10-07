96b8e47b631e1631cbd05bf3b19fff99437e658c
Ked57 <kedketh@gmail.com>
2019-10-07 20:03:47 +0200

    :sparkles: Autoscroll and props to change header text

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,24 @@ class MarcelPluginPublicContributions extends Marcel.Plugin {
   constructor() {
     super();
     this.root = document.getElementById("root");
+    this.interval = null;
+  }
+
+  autoScroll() {
+    this.interval = setInterval(() => {
+      window.scrollBy(0, 1);
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        clearInterval(this.interval);
+        window.scrollBy(0, -window.scrollY);
+        setTimeout(() => this.autoScroll(), 5000);
+      }
+    }, 50);
   }
 
   async render() {
     const {
       backend_url,
+      top_message,
       body_bg_color,
       body_txt_color,
       body_font,
@@ -46,14 +59,14 @@ class MarcelPluginPublicContributions extends Marcel.Plugin {
     const template = `
     <div class="container">
       <div class="header">
-        Public contributions at Zenika this month
+        ${top_message}
       </div>
       <div class="info-container">
         ${`<div class="row first">
-        <div class="row-item"><img class="thumbnail" src=${first.avatarUrl}></div>
-        <div class="row-item">${first.name}</div>
-        <div class="row-item">${first.count}</div>
-      </div>`}
+            <div class="row-item"><img class="thumbnail" src=${first.avatarUrl}></div>
+            <div class="row-item">${first.name}</div>
+            <div class="row-item">${first.count}</div>
+          </div>`}
         ${usableData
           .map(
             row =>
@@ -68,6 +81,8 @@ class MarcelPluginPublicContributions extends Marcel.Plugin {
     </div>`;
     this.root.innerHTML = template;
 
+    setTimeout(() => this.autoScroll(), 5000);
+
     // stylesvar is a special property containing the global media theme.
     // You should use it to have a consistent style accross all the media.
     if (stylesvar["primary-color"])
@@ -80,11 +95,12 @@ class MarcelPluginPublicContributions extends Marcel.Plugin {
 Marcel.init(MarcelPluginPublicContributions);
 
 // uncomment this line to try the plugin in a browser :
-// Marcel.changeProps({
-//   backend_url: "http://localhost:8080",
-//   body_bg_color: "#f2f2f2",
-//   body_txt_color: "#4c4c4c",
-//   body_font: "Helvetica, Arial, sans-serif",
-//   row_border_color: "#d2d6d8",
-//   first_txt_color: "#b51432"
-// });
+Marcel.changeProps({
+  backend_url: "http://localhost:8080",
+  body_bg_color: "#f2f2f2",
+  body_txt_color: "#4c4c4c",
+  body_font: "Helvetica, Arial, sans-serif",
+  row_border_color: "#d2d6d8",
+  first_txt_color: "#b51432",
+  top_message: "Public contributions of Zenika Members during hacktoberfest !"
+});
